8c8585a485da08cfb3888f4e060ad58b37338728
speedy <k.w1@o2.pl>
2020-07-28 00:54:46 +0200

    Modify callbacks

--- inc/Api/SettingsApi.php
+++ inc/Api/SettingsApi.php
@@ -8,10 +8,16 @@ namespace Inc\Api;
 class SettingsApi {
     public $admin_pages = array();
     public $admin_subpages = array();
+    public $settings = array();
+    public $sections = array();
+    public $fields = array();
 
     public function register() {
         if(!empty($this->admin_pages)) {
             add_action('admin_menu', array($this, 'add_admin_menu'));
+        }
+        if(!empty($this->settings)) {
+            add_action('admin_init', array($this, 'add_admin_menu'));
         }
     }
 
@@ -58,6 +64,51 @@ class SettingsApi {
             add_submenu_page(
                 $page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'],
                 $page['menu_slug'], $page['callback']
+            );
+        }
+    }
+
+    public function set_settings(array $settings) {
+        $this->$settings = $settings;
+        return $this;
+    }
+
+    public function set_sections(array $sections) {
+        $this->$sections = $sections;
+        return $this;
+    }
+
+    public function set_fields(array $fields) {
+        $this->$fields = $fields;
+        return $this;
+    }
+
+    public function register_custom_fields() {
+        foreach ($this->settings as $setting) {
+            register_setting(
+                $setting["option_group"],
+                $setting["option_name"],
+                (isset ($setting["callback"])) ? $setting["callback"] : ''
+            );
+        }
+
+        foreach ($this->sections as $section) {
+            add_settings_section(
+                $section["id"],
+                $section["title"],
+                (isset ($section["callback"]) ? $section["callback"] : ''),
+                $section["page"]
+            );
+        }
+
+        foreach ($this->fields as $field) {
+            add_settings_field(
+                $field["id"],
+                $field["title"],
+                (isset ($field["callback"]) ? $field["callback"] : ''),
+                $field["page"],
+                $field["section"],
+                (isset ($field["args"]) ? $section["args"] : '')
             );
         }
     }
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -7,31 +7,49 @@
 
 namespace Inc\Pages;
 
-use \Inc\Base\BaseController;
 use \Inc\Api\SettingsApi;
+use \Inc\Base\BaseController;
+use \Inc\Api\Callbacks\AdminCallbacks;
 
 class Admin extends BaseController{
     public $settings;
     public $pages = array();
     public $subpages = array();
+    public $callbacks;
 
-    public function __construct() {
+    public function register() {
         $this->settings = new SettingsApi();
+        $this->callbacks = new AdminCallbacks();
 
+        $this->set_pages();
+        $this->set_subpages();
+
+        $this->set_settings();
+        $this->set_sections();
+        $this->set_fields();
+
+        $this->settings
+            ->add_pages($this->pages)
+            ->with_subpage('Option 0')
+            ->add_subpages($this->subpages)
+            ->register();
+    }
+
+    public function set_pages() {
         $this -> pages = [
             [
                 'page_title' => 'SupperBrutto',
                 'menu_title' =>'SupperBrutto',
                 'capability' => 'manage_options',
                 'menu_slug' => 'supperbrutto',
-                'callback' => function() {
-                    echo '<h1>Helloo!</h1>';
-                },
+                'callback' => array($this->callbacks, 'admin_dashboard'),
                 'icon_url' => 'dashicons-money',
                 'position' => 110
             ]
         ];
+    }
 
+    public function set_subpages() {
         $this -> subpages = [
             [
                 'parent_slug' => 'supperbrutto',
@@ -39,9 +57,7 @@ class Admin extends BaseController{
                 'menu_title' => 'Option 1',
                 'capability' => 'manage_options',
                 'menu_slug' => 'supperbrutto_cpt',
-                'callback' => function() {
-                    echo '<h1>Option1 Hello!</h1>';
-                }
+                'callback' => array($this->callbacks, 'option1_callback')
             ],
             [
                 'parent_slug' => 'supperbrutto',
@@ -49,18 +65,48 @@ class Admin extends BaseController{
                 'menu_title' => 'Option 2',
                 'capability' => 'manage_options',
                 'menu_slug' => 'supperbrutto_widget',
-                'callback' => function() {
-                    echo '<h1>Option2 Hello!</h1>';
-                }
+                'callback' => array($this->callbacks, 'option2_callback')
             ]
         ];
     }
 
-    public function register() {
-        $this->settings
-            ->add_pages($this->pages)
-            ->with_subpage('Option 0')
-            ->add_subpages($this->subpages)
-            ->register();
+    public function set_settings() {
+        $args = [
+            [
+                'option_group' => 'supperbrutto_group',
+                'option_name' => 'text_example',
+                'callback' => array($this->callbacks, 'supperbrutto_option_groups')
+            ]
+        ];
+        $this->settings->set_settings($args);
+    }
+
+    public function set_sections() {
+        $args = [
+            [
+                'id' => 'supperbrutto_admin_index',
+                'title' => 'Settings',
+                'callback' => array($this->callbacks, 'supperbrutto_admin_section'),
+                'page' => 'supperbrutto'
+            ]
+        ];
+        $this->settings->set_sections($args);
+    }
+
+    public function set_fields() {
+        $args = [
+            [
+                'id' => 'text_example',
+                'title' => 'Text Example',
+                'callback' => array($this->callbacks, 'supperbrutto_text_example'),
+                'page' => 'supperbrutto',
+                'section' => 'supperbrutto_admin_index',
+                'args' => [
+                    'label_for' => 'text_example',
+                    'class' => 'example-class'
+                ]
+            ]
+        ];
+        $this->settings->set_fields($args);
     }
 }
